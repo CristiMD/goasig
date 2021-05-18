874e73125a7ff46dbdb58efcff59a62b26f09380
Mihai Cristian <mihai.cristian.d@gmail.com>
2021-05-18 12:31:43 +0300

    fix linkuri dinamice contul meu

--- resources/views/cont/nav.blade.php
+++ resources/views/cont/nav.blade.php
@@ -1,10 +1,10 @@
 <div class="account-nav">
     <ul>
-        <li><a class="{{$activ == 'detalii' ? 'activ' : ''}}" href="/contul-meu">Detalii personale</a></li>
-        <li><a class="{{$activ == 'oferte' ? 'activ' : ''}}" href="/contul-meu/oferte">Oferte disponibile</a></li>
-        <li><a class="{{$activ == 'polite' ? 'activ' : ''}}" href="/contul-meu/polite">Polite inregistrate</a></li>
-        <li><a class="{{$activ == 'vehicule' ? 'activ' : ''}}" href="/contul-meu/vehicule">Vehicule inregistrate</a></li>
-        <li><a class="{{$activ == 'proprietari' ? 'activ' : ''}}" href="/contul-meu/proprietari">Proprietari inregistrati</a></li>
-        <li><a class="{{$activ == 'conducatori' ? 'activ' : ''}}" href="/contul-meu/conducatori">Conducatori inregistrati</a></li>
+        <li><a class="{{$activ == 'detalii' ? 'activ' : ''}}" href="{{ config('app.url')}}contul-meu">Detalii personale</a></li>
+        <li><a class="{{$activ == 'oferte' ? 'activ' : ''}}" href="{{ config('app.url')}}contul-meu/oferte">Oferte disponibile</a></li>
+        <li><a class="{{$activ == 'polite' ? 'activ' : ''}}" href="{{ config('app.url')}}contul-meu/polite">Polite inregistrate</a></li>
+        <li><a class="{{$activ == 'vehicule' ? 'activ' : ''}}" href="{{ config('app.url')}}contul-meu/vehicule">Vehicule inregistrate</a></li>
+        <li><a class="{{$activ == 'proprietari' ? 'activ' : ''}}" href="{{ config('app.url')}}contul-meu/proprietari">Proprietari inregistrati</a></li>
+        <li><a class="{{$activ == 'conducatori' ? 'activ' : ''}}" href="{{ config('app.url')}}contul-meu/conducatori">Conducatori inregistrati</a></li>
     </ul>
 </div>
