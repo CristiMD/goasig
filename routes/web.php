e5e45ccbab1cdb3eeb486c137fad0920569f8581
Mihai Cristian <mihai.cristian.d@gmail.com>
2021-05-18 13:44:41 +0300

    fix linkuri dinamice contul meu

--- routes/web.php
+++ routes/web.php
@@ -69,3 +69,7 @@ Route::post('vehicul/{nr_inmatriculare}', [App\Http\Controllers\VehiculControlle
 Route::get('/contul-meu/{view?}', [App\Http\Controllers\UsersController::class, 'cont']);
 
 Route::get('/save-pdf/{id_oferta}',[App\Http\Controllers\PoliteController::class, 'save_pdf']);
+
+Route::get('/foo', function () {
+    Artisan::call('storage:link');
+});
