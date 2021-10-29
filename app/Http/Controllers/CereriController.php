28ce3bcf4c10be17f92718243732b0fcc4eaebb0
Mihai Cristian <mihai.cristian.d@gmail.com>
2021-10-29 11:23:39 +0300

    SOAP parse metoda caen IP set

--- app/Http/Controllers/CereriController.php
+++ app/Http/Controllers/CereriController.php
@@ -958,7 +958,7 @@ class CereriController extends Controller
 
         try {
             $data = $client->get_coduri_caen();
-            foreach (data as $key => $value) {
+            foreach ($data as $key => $value) {
                 $tmp = new \stdClass();
                 $tmp->cod = $value["cod"][0];
                 $tmp->nume = $value["nume"][0];
